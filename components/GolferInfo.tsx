f984abe89261e183f032e8d81908db82ff998b09
Jon Bake <jonmbake@gmail.com>
2023-03-05 11:55:01 -0600

    Use one based golfer index in golfer info

--- components/GolferInfo.tsx
+++ components/GolferInfo.tsx
@@ -11,11 +11,11 @@ export default function GolferInfo( { numberOfGolfers }: Props) {
           <h3>Golfer #{index + 1}</h3>
           <div key={index} className="mb-3">
             <label className="form-label">Full Name</label>
-            <input type="text" required={true} className="form-control" name={"golfer" + index + "_name"} />
+            <input type="text" required={true} className="form-control" name={"golfer" + (index + 1) + "_name"} />
           </div>
           <div key={index} className="mb-3">
             <label className="form-label">Email</label>
-            <input type="email" required={true} className="form-control" name={"golfer" + index + "_email"} />
+            <input type="email" required={true} className="form-control" name={"golfer" + (index + 1) + "_email"} />
             <div className="form-text">Email address will be used to send golf outing updates.</div>
           </div>
           </>
